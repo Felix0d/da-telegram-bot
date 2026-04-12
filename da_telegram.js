97ca93346b1caf84eb794ef3d64344f531fc72a4
Felix0d <142845604+Felix0d@users.noreply.github.com>
2026-04-13 02:37:49 +0300

    Update da_telegram.js

--- da_telegram.js
+++ da_telegram.js
@@ -1,40 +1,33 @@
-// Берем токены из настроек Render (Environment Variables)
 const telegramToken = process.env.TELEGRAM_TOKEN;
 const daToken = process.env.DA_TOKEN;
 const channel = process.env.TELEGRAM_CHANNEL;
 
-let eventId = null;
+// --- Добавляем мини-сервер для UptimeRobot ---
+const http = require('http');
+http.createServer((req, res) => {
+  res.write("I am alive!");
+  res.end();
+}).listen(process.env.PORT || 3000); 
+// --------------------------------------------
 
+let eventId = null;
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(telegramToken);
 
-bot.launch(
-  {dropPendingUpdates: true,},
-  console.log("Telegram bot started on Cloud")
-);
+bot.launch({dropPendingUpdates: true}, console.log("Bot is online!"));
 
 const socket = require('socket.io-client')
-  .connect("wss://socket.donationalerts.ru:443",
-  { transports: ["websocket"] },
-  {reconnection: true,
-  reconnectionDelay: 1000,
-  reconnectionDelayMax: 5000,
-  reconnectionAttempts: Infinity
-  });
+  .connect("wss://socket.donationalerts.ru:443", 
+  { transports: ["websocket"], reconnection: true });
 
 socket.emit('add-user', { token: daToken, type: "minor" });
-
-socket.on('connect', function(data){
-  console.log('Connected to Donation Alerts');
-});
 
 socket.on('donation', function(msg){
   let event = JSON.parse(msg);
   if (event.alert_type === '1' || event.alert_type === 1) {
-    if (eventId === event.id) {return}
-    if (event.username === null){ event.username = 'Аноним'}
-    eventId = event.id
-    console.log(`${event.username} донатит ${event.amount_formatted} ${event.currency}`);
-    bot.telegram.sendMessage(channel, `${event.username} донатит ${event.amount_formatted} ${event.currency} и говорит: "${event.message}"`);
+    if (eventId === event.id) return;
+    if (event.username === null) event.username = 'Аноним';
+    eventId = event.id;
+    bot.telegram.sendMessage(channel, `${event.username} донатит ${event.amount_formatted} ${event.currency}\n"${event.message}"`);
   }
 });
